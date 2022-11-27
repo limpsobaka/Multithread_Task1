6f831fc59aa105ecc2682a0afb1250c3192886de
Limpsobaka <limpsobaka@yandex.ru>
2022-11-27 18:55:29 +0300

    Change main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,47 +1,55 @@
 import java.util.*;
+import java.util.concurrent.*;
 
 public class Main {
 
-  public static void main(String[] args) throws InterruptedException {
-    List<Thread> threads = new ArrayList<>();
+  public static void main(String[] args) throws InterruptedException, ExecutionException {
+    List<Future> futureList = new ArrayList<>();
+    ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
-    String[] texts = new String[25];
-    for (int i = 0; i < texts.length; i++) {
-      texts[i] = generateText("aab", 30_000);
-    }
-
-    long startTs = System.currentTimeMillis(); // start time
-    for (String text : texts) {
-      Runnable logic = () -> {
-        int maxSize = 0;
-        for (int i = 0; i < text.length(); i++) {
-          for (int j = 0; j < text.length(); j++) {
-            if (i >= j) {
-              continue;
-            }
-            boolean bFound = false;
-            for (int k = i; k < j; k++) {
-              if (text.charAt(k) == 'b') {
-                bFound = true;
-                break;
+    try {
+      String[] texts = new String[25];
+      for (int i = 0; i < texts.length; i++) {
+        texts[i] = generateText("aab", 30_000);
+      }
+      long startTs = System.currentTimeMillis(); // start time
+      for (String text : texts) {
+        Callable logic = () -> {
+          int maxSize = 0;
+          for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < text.length(); j++) {
+              if (i >= j) {
+                continue;
+              }
+              boolean bFound = false;
+              for (int k = i; k < j; k++) {
+                if (text.charAt(k) == 'b') {
+                  bFound = true;
+                  break;
+                }
+              }
+              if (!bFound && maxSize < j - i) {
+                maxSize = j - i;
               }
             }
-            if (!bFound && maxSize < j - i) {
-              maxSize = j - i;
-            }
           }
-        }
-        System.out.println(text.substring(0, 100) + " -> " + maxSize);
-      };
-      Thread thread = new Thread(logic);
-      thread.start();
-      threads.add(thread);
+          System.out.println(text.substring(0, 100) + " -> " + maxSize);
+          return maxSize;
+        };
+        futureList.add(executor.submit(logic));
+      }
+
+      int maxSizeValue = 0;
+      for (Future f : futureList) {
+        maxSizeValue = (Integer) f.get() > maxSizeValue ? (Integer) f.get() : maxSizeValue;
+      }
+
+      System.out.println("Maximal size: " + maxSizeValue);
+      long endTs = System.currentTimeMillis(); // end time
+      System.out.println("Time: " + (endTs - startTs) + "ms");
+    } finally {
+      executor.shutdown();
     }
-    for (Thread thread : threads) {
-      thread.join();
-    }
-    long endTs = System.currentTimeMillis(); // end time
-    System.out.println("Time: " + (endTs - startTs) + "ms");
   }
 
   public static String generateText(String letters, int length) {
